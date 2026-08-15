f64b8a0479f9de746da77594df5bc774009f4953
Vince Odhiambo <architectvince7@gmail.com>
2026-08-15 20:28:27 +0300

    feat: boot authenticated API-backed EMS app

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { BrowserRouter } from 'react-router-dom'
 import { Toaster } from 'sonner'
-import App from './App'
+import App from './AppFull'
 import './styles.css'
 import './ui/premium.css'
 
